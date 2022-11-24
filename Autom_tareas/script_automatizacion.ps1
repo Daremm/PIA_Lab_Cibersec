6457cb9c4a1861f16cfa52b7466d41e404cce57f
Daremm <111472552+Daremm@users.noreply.github.com>
2022-11-23 19:01:00 -0600

    Add files via upload

---
+++ Autom_tareas/script_automatizacion.ps1
@@ -0,0 +1,5 @@
+﻿$Path = (get-command powershell.exe).Path
+$tarea=New-ScheduledTaskAction -Execute $Path -Argument 'C:\Users\danie\OneDrive\Documentos\PS Scripts\send_sysinfo.ps1' -WorkingDirectory "C:\Windows\System32"
+$trigger=New-ScheduledTaskTrigger -Once -At 2:49pm
+
+Register-ScheduledTask SENDDSYSINFO1 -RunLevel Highest -Trigger $trigger -Action $tarea -TaskPath "MisTareas"
