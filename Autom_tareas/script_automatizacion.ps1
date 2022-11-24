999de058d1d773f3ecbdaf81cfc268f5e7ce40e7
Daremm <111472552+Daremm@users.noreply.github.com>
2022-11-24 13:34:54 -0600

    Se ocultó la ruta

--- Autom_tareas/script_automatizacion.ps1
+++ Autom_tareas/script_automatizacion.ps1
@@ -1,5 +1,10 @@
-﻿$Path = (get-command powershell.exe).Path
-$tarea=New-ScheduledTaskAction -Execute $Path -Argument 'C:\Users\danie\OneDrive\Documentos\PS Scripts\send_sysinfo.ps1' -WorkingDirectory "C:\Windows\System32"
+# Daniel Arreaga Escareño - 2029652
+# Fecha: 22/11/2022
+#
+# NOTA: Se debe cambiar la hora y ruta
+#
+$Path = (get-command powershell.exe).Path
+$tarea=New-ScheduledTaskAction -Execute $Path -Argument 'C:\ruta\send_sysinfo.ps1' -WorkingDirectory "C:\Windows\System32"
 $trigger=New-ScheduledTaskTrigger -Once -At 2:49pm
 
 Register-ScheduledTask SENDDSYSINFO1 -RunLevel Highest -Trigger $trigger -Action $tarea -TaskPath "MisTareas"
