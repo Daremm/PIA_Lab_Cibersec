bd7b70f08d025aa6907c57e02496aa94e5e28df7
Daremm <111472552+Daremm@users.noreply.github.com>
2022-11-24 12:06:59 -0600

    Se ocultaron rutas

--- Encode_Decode/codipo.ps1
+++ Encode_Decode/codipo.ps1
@@ -1,11 +1,12 @@
-﻿#30/09/2022 - Daniel Arreaga Escareño
+#30/09/2022 - Daniel Arreaga Escareño
+# NOTA: Poner su ruta donde diga #ruta
 Clear-Host
 Write-Host "Bienvenido a un ejemplo de codificación/decodificación base64 usando powershell"-ForegroundColor Green
 Write-Host "Codificando un archivo de texto"
 #
 #Se va a leer el contenido del archivo de texto
 #
-$inputfile = 'C:\Users\danie\OneDrive\Documentos\PS Scripts\secret.txt'
+$inputfile = #ruta
 $fc = Get-Content $inputfile
 $GB = [System.Text.Encoding]::UTF8.GetBytes($fc)
 $etext = [System.Convert]::ToBase64String($GB)
@@ -15,6 +16,6 @@ Write-Host "El contenido del archivo CODIFICADO es: " $etext -ForegroundColor Gr
 #
 Write-Host "DECODIFICANDO el texto previo:"
 [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($etext)) | Out-File -Encoding "ASCII" 'C:\Users\danie\OneDrive\Documentos\PS Scripts\decode_secret.txt'
-$outfile2 = Get-Content 'C:\Users\danie\OneDrive\Documentos\PS Scripts\decode_secret.txt'
+$outfile2 = Get-Content #ruta
 Write-Host "El texto decodificado es el siguiente:"-ForegroundColor Green
 Write-Host "DECODIFICADO: " $outfile2
